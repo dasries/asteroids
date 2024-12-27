666fbab91cf641a4b7106dd41750c31f721024de
Sebastian Ries <da.s.ries@googlemail.com>
2024-12-27 13:51:30 +0100

    feat: explosion animation

--- asteroid.py
+++ asteroid.py
@@ -8,7 +8,10 @@ class Asteroid(CircleShape):
         super().__init__(x, y, radius)
         image = pygame.image.load("assets/asteroid.png")
         self.image = pygame.transform.scale(image, (radius, radius))
+        self.explosion_image = pygame.image.load("assets/explode.png")
         self.rotation = 0
+        self.explode = False
+        self.explosion_timer = 0
         
     def draw(self, screen):
         rotated_image = pygame.transform.rotate(self.image, self.rotation)
@@ -16,11 +19,16 @@ class Asteroid(CircleShape):
         screen.blit(rotated_image, new_rect)
     
     def update(self, dt, _):
+        if self.explode:
+            self.explosion_timer += dt
+            self.image = pygame.transform.scale(self.explosion_image, (self.radius * self.explosion_timer * 10, self.radius * self.explosion_timer * 10))
+            if self.explosion_timer > 0.25:
+                self.kill()
         self.position += self.velocity * dt
         self.rotation += random.randint(20, 150) * dt
         
     def split(self):
-        self.kill()
+        self.explode = True
         if self.radius <= ASTEROID_MIN_RADIUS:
             return 1000
         angle = random.uniform(20, 50)
@@ -29,6 +37,7 @@ class Asteroid(CircleShape):
         new_radius = self.radius - ASTEROID_MIN_RADIUS
         Asteroid(self.position.x, self.position.y, new_radius).velocity = vec_one * 1.2
         Asteroid(self.position.x, self.position.y, new_radius).velocity = vec_two * 1.2
-        return 100    
+        return 100  
+       
         
         
